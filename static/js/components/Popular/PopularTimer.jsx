a2808fd0073674b1c2be850597872e4bb302106e
0648596375 <tbinetruy@gmail.com>
2017-01-17 20:37:46 +0100

    fixed timeout state issues when unmounting

--- static/js/components/Popular/PopularTimer.jsx
+++ static/js/components/Popular/PopularTimer.jsx
@@ -12,6 +12,10 @@ var PopularTimer = React.createClass({
 		}
 	},
 
+	componentWillUnmount: function() {
+		window.clearTimeout(this.timeout)
+	},
+
 	getHashtag: function() {
 		const hastagNames = discoverHashtagNames()
 		const discoverNumer = getDiscoverNumberFromCurrentTime()
@@ -29,7 +33,7 @@ var PopularTimer = React.createClass({
 	render: function() {
 		// event loop hack
 		// update state.time evey 10 sec
-		window.setTimeout(() => this.setState({ time: new Date() }), 10*1000)
+		this.timeout = window.setTimeout(() => this.setState({ time: new Date() }), 10*1000)
 
 		return (
 			<div className="popular-timer--wrapper">
