a04f8da83d3c344922cf1f2c1e194a8d2b2f607a
0648596375 <tbinetruy@gmail.com>
2017-05-08 22:24:58 +0200

    finished desktop upload loop

--- static/js/components/UploadSC/UploadSC.jsx
+++ static/js/components/UploadSC/UploadSC.jsx
@@ -165,7 +165,7 @@ var UploadSC = React.createClass({
 			} else {
 				content = <UploadSCHome
 					getUserTracks={ this.getUserTracks } />
-				title = 'Get Featured on the Crowdradio!'
+				title = 'Get Featured on MASAS!'
 				pageNumber = 1
 			}
 		}
