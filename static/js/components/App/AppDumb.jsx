4c18b8ffec31342d7c5e5e898e2bd2417f7b3d2e
0648596375 <tbinetruy@gmail.com>
2017-05-19 23:21:34 +0200

    updaetd format

--- static/js/components/App/AppDumb.jsx
+++ static/js/components/App/AppDumb.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { PropTypes } from 'react'
 import { connect }from 'react-redux'
 
 import { closeAndEmptyMainModal } from '../../reducers/actions/App.js'
@@ -35,108 +35,105 @@ const mapDispatchToProps = function(dispatch) {
 /**
  * Smart component
  */
-class AppDumbClass extends React.Component {
-	getDefaultProps() {
-		return {
-			hideLoadingModalZIndex: 100,
-			loadingModalAnim: 'fadeout-loading-modal 1s linear',
-		}
+
+const smartPropTypes = {
+	bgFilter: PropTypes.object,
+	children: PropTypes.element,
+	closeModal: PropTypes.func,
+	hideLoadingModalZIndex: PropTypes.number,			// loading modal z-index
+	isModalOpened: PropTypes.bool,
+	loadingModalAnim: PropTypes.string,				// loading modal animation
+	modalContent: PropTypes.element,
+	modalType: PropTypes.number,
+}
+
+const smartDefaultProps = {
+	hideLoadingModalZIndex: 100,
+	loadingModalAnim: 'fadeout-loading-modal 1s linear',
+}
+
+const AppDumbClass = props => {
+	const authCookieFrontgroundStyle = {
+		position: 'fixed',
+		top: 0,
+		bottom: 0,
+		left: 0,
+		right: 0,
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: 'black',
+		color: 'white',
+		opacity: props.hideLoadingModalZIndex ? 0 : 1,
+		animation: props.loadingModalAnim,
+		pointerEvents: props.hideLoadingModalZIndex > 0 ? 'default' : 'none'
 	}
 
-	componentWillMount() {
-	}
-
-	render() {
-		const authCookieFrontgroundStyle = {
-			position: 'fixed',
-			top: 0,
-			bottom: 0,
-			left: 0,
-			right: 0,
-			display: 'flex',
-			justifyContent: 'center',
-			alignItems: 'center',
-			backgroundColor: 'black',
-			color: 'white',
-			opacity: this.props.hideLoadingModalZIndex ? 0 : 1,
-			animation: this.props.loadingModalAnim,
-			pointerEvents: this.props.hideLoadingModalZIndex > 0 ? 'default' : 'none'
-		}
-
-		return (
-			<NavSidebar>
-				<div style = { styles.container } id="mobile-safari-bug-fix--wrapper" >
-					<div
-						className={
-							'body--background'
-							+
-							( this.props.bgFilter.blurred ? ' blurred ' : '' )
-							+
-							( this.props.bgFilter.saturated ? ' saturated ' : '' )
-							+
-							( this.props.bgFilter.mobileBlurred ? ' blurred-mobile ' : '' )
-							+
-							( this.props.bgFilter.mobileSaturated ? ' mobileSaturated ' : '' )
-							+
-							( this.props.bgFilter.modalBlurred ? ' modal-blurred ' : '' )
-							+
-							( this.props.bgFilter.modalSaturated ? ' modal-saturated ' : '' )
-							+
-							( this.props.bgFilter.black ? ' black ' : '' )
-						}
-						id="body--background">
-						<div className="bg-image" id="app-bg-image"></div>
-					</div>
-					<Header />
-						<div
-							className={ 'modal-blur--wrapper' + ( this.props.isModalOpened && this.props.modalType !== 2 ? ' blurred' : '' )}
-							style={{
-								opacity: !(this.props.isModalOpened && this.props.modalType === 4) ? 1 : 0,
-							}}>
-							{
-								this.props.children ?
-									this.props.children
-								:
-									<Discover />
-							}
-						</div>
-						<div
-							style={ authCookieFrontgroundStyle }>
-							<img
-								style={{
-									height: '7rem',
-									width: '7rem',
-								}}
-								src="/static/img/MASAS_logo-M.svg"
-								alt="loading" />
-						</div>
-
-					<Footer />
-
+	return (
+		<NavSidebar>
+			<div style = { styles.container } id="mobile-safari-bug-fix--wrapper" >
+				<div
+					className={
+						'body--background'
+						+
+						( props.bgFilter.blurred ? ' blurred ' : '' )
+						+
+						( props.bgFilter.saturated ? ' saturated ' : '' )
+						+
+						( props.bgFilter.mobileBlurred ? ' blurred-mobile ' : '' )
+						+
+						( props.bgFilter.mobileSaturated ? ' mobileSaturated ' : '' )
+						+
+						( props.bgFilter.modalBlurred ? ' modal-blurred ' : '' )
+						+
+						( props.bgFilter.modalSaturated ? ' modal-saturated ' : '' )
+						+
+						( props.bgFilter.black ? ' black ' : '' )
+					}
+					id="body--background">
+					<div className="bg-image" id="app-bg-image"></div>
 				</div>
-				<PlayerAudioTag />
-				<Modal
-					isOpened={ this.props.isModalOpened }
-					closeModalFunc={ this.props.closeModal }
-					type={ this.props.modalType }>
-					{ this.props.modalContent }
-				</Modal>
-				<PlayerMobile />
-			</NavSidebar>
-		)
-	}
+				<Header />
+					<div
+						className={ 'modal-blur--wrapper' + ( props.isModalOpened && props.modalType !== 2 ? ' blurred' : '' )}
+						style={{
+							opacity: !(props.isModalOpened && props.modalType === 4) ? 1 : 0,
+						}}>
+						{
+							props.children ?
+								props.children
+							:
+								<Discover />
+						}
+					</div>
+					<div
+						style={ authCookieFrontgroundStyle }>
+						<img
+							style={{
+								height: '7rem',
+								width: '7rem',
+							}}
+							src="/static/img/MASAS_logo-M.svg"
+							alt="loading" />
+					</div>
+
+				<Footer />
+
+			</div>
+			<PlayerAudioTag />
+			<Modal
+				isOpened={ props.isModalOpened }
+				closeModalFunc={ props.closeModal }
+				type={ props.modalType }>
+				{ props.modalContent }
+			</Modal>
+			<PlayerMobile />
+		</NavSidebar>
+	)
 }
 
-AppDumbClass.propTypes = {
-	bgFilter: React.PropTypes.object,
-	children: React.PropTypes.element,
-	closeModal: React.PropTypes.func,
-	hideLoadingModalZIndex: React.PropTypes.number,			// loading modal z-index
-	isModalOpened: React.PropTypes.bool,
-	loadingModalAnim: React.PropTypes.string,				// loading modal animation
-	modalContent: React.PropTypes.element,
-	modalType: React.PropTypes.number,
-}
+AppDumbClass.propTypes = smartPropTypes
+AppDumbClass.defaultProps = smartDefaultProps
 
 
 const styles = {
