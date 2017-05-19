1600576f439504f321583570d800ef0e66debe1f
0648596375 <tbinetruy@gmail.com>
2017-05-19 22:33:47 +0200

    deleted body because it's in UI/

--- static/js/components/Body/containers/Body.jsx
+++
@@ -1,8 +0,0 @@
-
-// var Body = require('../../components/Body/Body.jsx')
-
-var Body = {}
-
-// Which part of the Redux global state does our component want to receive as props?
-
-module.exports = Body
