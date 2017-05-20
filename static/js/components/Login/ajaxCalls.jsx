3b739ff1dd23ad1154d64af5b4268746f2fd2eb9
0648596375 <tbinetruy@gmail.com>
2017-05-20 14:36:08 +0200

    linting

--- static/js/components/Login/ajaxCalls.jsx
+++ static/js/components/Login/ajaxCalls.jsx
@@ -1,56 +1,56 @@
-var { browserHistory } = require("react-router")
+var { browserHistory } = require('react-router')
 
-const { dispatch } = require("../../reducers/reducers.js")
-var { updateAuthCookie, logInWithToken } = require("../../MASAS_functions.jsx")
+const { dispatch } = require('../../reducers/reducers.js')
+var { updateAuthCookie, logInWithToken } = require('../../MASAS_functions.jsx')
 
 var ajaxCalls = {}
 
 ajaxCalls.acceptTerms = (userToken, userData, userPk) => {
-	var header = "Bearer " + userToken
+	var header = 'Bearer ' + userToken
 
 	$.ajax({
-		type: "POST",
-		url: "/api/usersteps/",
+		type: 'POST',
+		url: '/api/usersteps/',
 		headers: {
-			"Authorization": header,
+			'Authorization': header,
 		},
 		data: {
 			user: userData.url,
 			step: 1,
 		},
 		success: () => {
-			dispatch({ type: "UPDATE_USER_PK", pk: userPk })
-			dispatch({ type: "LOGIN", token: userToken, userData , pk: userPk })
-			dispatch({ type: "TOOGLE_IS_MODAL_OPENED" })
-			dispatch({ type: "UPDATE_NOTIFICATION_TEXT", notificationText: "" })
-			dispatch({ type: "UPDATE_NOTIFICATION_TEXT", notificationText: "Welcome !" })
-			
-			browserHistory.push("/profile")
+			dispatch({ type: 'UPDATE_USER_PK', pk: userPk })
+			dispatch({ type: 'LOGIN', token: userToken, userData , pk: userPk })
+			dispatch({ type: 'TOOGLE_IS_MODAL_OPENED' })
+			dispatch({ type: 'UPDATE_NOTIFICATION_TEXT', notificationText: '' })
+			dispatch({ type: 'UPDATE_NOTIFICATION_TEXT', notificationText: 'Welcome !' })
+
+			browserHistory.push('/profile')
 		},
 		error: () => {
-			dispatch({ type: "UPDATE_NOTIFICATION_TEXT", notificationText: "" })
-			dispatch({ type: "UPDATE_NOTIFICATION_TEXT", notificationText: "Welcome !" })
+			dispatch({ type: 'UPDATE_NOTIFICATION_TEXT', notificationText: '' })
+			dispatch({ type: 'UPDATE_NOTIFICATION_TEXT', notificationText: 'Welcome !' })
 		}
 	})
 }
 
-// (obj) userDict => userDict.userToken and userDict.userPk 
+// (obj) userDict => userDict.userToken and userDict.userPk
 ajaxCalls.updateProfilePicture = (userDict) => {
 	const userToken = userDict.userToken
 	const userPk = userDict.userPk
 
-	var header = "Bearer " + userToken
+	var header = 'Bearer ' + userToken
 
-	if(typeof(FB) !== "undefined")
+	if(typeof(FB) !== 'undefined')
 		$.ajax({
-			type: "PATCH",
-			url: "/api/users/" + userPk + "/",
+			type: 'PATCH',
+			url: '/api/users/' + userPk + '/',
 			headers: {
-				"Authorization": header,
-				"Content-Type": "application/json"
+				'Authorization': header,
+				'Content-Type': 'application/json'
 			},
 			data: JSON.stringify({
-				avatar_url: "https://graph.facebook.com/v2.5/" + FB.getUserID() + "/picture",
+				avatar_url: 'https://graph.facebook.com/v2.5/' + FB.getUserID() + '/picture',
 			}),
 			success: () => { },
 			error: () => { }
@@ -61,38 +61,38 @@ ajaxCalls.updateProfilePicture = (userDict) => {
 
 ajaxCalls.convertToken = () => {
 	$.ajax({
-		type: "POST",
-		url: "/auth/convert-token/",
+		type: 'POST',
+		url: '/auth/convert-token/',
 		data: {
-			grant_type: "convert_token",
-			client_id: "biHRTlM74WJ2l8NddjR6pa8uNYpWC4vFzTjyjOUO",
-			client_secret: "aNXFRxyW20wBDLmTlf4ntmFKYSQ7qvig3PSRLlSxBYfxpmFPnh9JJz876eLMIeZJaoYyM2F6Q7q36QveAWacmiOT14y1z0EwpqO7lQVhXBx037FNGr6mDwYNq1fGfNVl",
-			backend: "facebook",
+			grant_type: 'convert_token',
+			client_id: 'biHRTlM74WJ2l8NddjR6pa8uNYpWC4vFzTjyjOUO',
+			client_secret: 'aNXFRxyW20wBDLmTlf4ntmFKYSQ7qvig3PSRLlSxBYfxpmFPnh9JJz876eLMIeZJaoYyM2F6Q7q36QveAWacmiOT14y1z0EwpqO7lQVhXBx037FNGr6mDwYNq1fGfNVl',
+			backend: 'facebook',
 			token: FB.getAccessToken(),
 		},
-		success: (data) => { 
+		success: (data) => {
 			logInWithToken(dispatch, data.access_token)
-			ajaxCalls.getUserPk(data.access_token, ajaxCalls.updateProfilePicture)	
+			ajaxCalls.getUserPk(data.access_token, ajaxCalls.updateProfilePicture)
 			updateAuthCookie(data.access_token)
 		},
-		error: () => { 
-			dispatch({type:"LOGOUT"})
+		error: () => {
+			dispatch({type:'LOGOUT'})
 		}
 	})
 }
 
 ajaxCalls.getUserPk = (userToken, callbackFunc = null) => {
-	var header = "Bearer " + userToken
+	var header = 'Bearer ' + userToken
 	$.ajax({
-		type: "GET",
-		url: "/api/check-user/",	
+		type: 'GET',
+		url: '/api/check-user/',
 		headers: {
-			"Authorization": header,
+			'Authorization': header,
 		},
 		success: (data) => {
 			var pk = data.userPk
 
-			dispatch({type: "UPDATE_USER_PK", pk: pk})
+			dispatch({type: 'UPDATE_USER_PK', pk: pk})
 
 			if(callbackFunc)
 				callbackFunc({ userToken, userPk: data.userPk})
