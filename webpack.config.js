d658463141912f097fa4a7cea557459936e90296
0648596375 <tbinetruy@gmail.com>
2017-05-21 13:11:32 +0200

    switched to webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,14 @@
-//var path = require('path')
-var webpack = require('webpack')
+const path = require('path');
 
-console.log(__dirname + '/static/js')
 module.exports = {
-	entry:  __dirname + '/static/js/index.jsx',
+	entry: './static/js/index.jsx',
 	output: {
-		path:     __dirname + 'static/js/builds',
-		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'static/js/dist'),
+		filename: 'my-first-webpack.bundle.js'
 	},
 	module: {
-		loaders: [
-			{
-				test: /\.jsx?/,
-				loader: 'babel',
-				exclude: __dirname + '/node_modules',
-				include: __dirname + '/static/js' 
-			}
-		 ],
-	}
+		rules: [
+			{test: /\.(js|jsx)$/, use: 'babel-loader'}
+		]
+  }
 };
