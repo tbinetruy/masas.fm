58171ea6e24d30938ae7b7ada6d281404a7c68e2
0648596375 <tbinetruy@gmail.com>
2017-05-28 14:43:18 +0200

    travis configs update

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 git status
+
 git add static/dist
 git status
+
 git stash
 git status
+
 git checkout production
 git status
+
 git stash pop
 git status
+
 git commit -m "build"
 git status
